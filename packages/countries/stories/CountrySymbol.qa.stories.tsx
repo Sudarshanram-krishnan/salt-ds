eaa37ab307a7ac2af89b4bb62b8cf3519db5680d
origami-z <davidcui2@me.com>
2024-03-08 17:14:54 +0000

    Countries no runtime CSS injection

--- packages/countries/stories/CountrySymbol.qa.stories.tsx
+++ packages/countries/stories/CountrySymbol.qa.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryFn } from "@storybook/react";
 import { StackLayout } from "@salt-ds/core";
 import { MX } from "@salt-ds/countries";
 import "@salt-ds/countries/saltCountries.css";
+import { QAContainer, QAContainerNoStyleInjection } from "docs/components";
 
 export default {
   title: "Country Symbols/Country Symbol/Country Symbol QA",
@@ -9,13 +10,15 @@ export default {
 
 export const CountrySymbolSizes: StoryFn = () => {
   return (
-    <StackLayout direction="row">
-      <MX size={1} />
-      <MX size={2} />
-      <MX size={3} />
-      <MX size={4} />
-      <MX size={5} />
-    </StackLayout>
+    <QAContainer height={500} width={1000} cols={4}>
+      <StackLayout direction="row">
+        <MX size={1} />
+        <MX size={2} />
+        <MX size={3} />
+        <MX size={4} />
+        <MX size={5} />
+      </StackLayout>
+    </QAContainer>
   );
 };
 
@@ -23,13 +26,38 @@ CountrySymbolSizes.parameters = {
   chromatic: { disableSnapshot: false },
 };
 
+export const NoStyleInjection: StoryFn = () => {
+  return (
+    <QAContainerNoStyleInjection
+      height={500}
+      width={1000}
+      cols={4}
+      enableStyleInjection={false}
+    >
+      <StackLayout direction="row">
+        <MX size={1} />
+        <MX size={2} />
+        <MX size={3} />
+        <MX size={4} />
+        <MX size={5} />
+      </StackLayout>
+    </QAContainerNoStyleInjection>
+  );
+};
+
+NoStyleInjection.parameters = {
+  chromatic: { disableSnapshot: false },
+};
+
 export const CssBackground: StoryFn = () => {
   return (
-    <StackLayout direction="row">
-      <div className="saltCountry-AD" />
-      <div className="saltCountry-MX" />
-      <div className="saltCountry-GB-SCT" />
-    </StackLayout>
+    <QAContainer height={500} width={1000} cols={4}>
+      <StackLayout direction="row">
+        <div className="saltCountry-AD" />
+        <div className="saltCountry-MX" />
+        <div className="saltCountry-GB-SCT" />
+      </StackLayout>
+    </QAContainer>
   );
 };
 
